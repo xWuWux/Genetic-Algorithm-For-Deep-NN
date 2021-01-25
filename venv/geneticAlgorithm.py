37b3cd444e4ec9c9a44d91ea3e9a7783fd57bd03
Francja <adambielewicz92@gmail.com>
2021-01-25 23:38:42 +0100

    simple mutation added

--- venv/geneticAlgorithm.py
+++ venv/geneticAlgorithm.py
@@ -9,6 +9,7 @@ MAX_NEURONS = 128
 POPULATION = 40 #need to be dividable by 4
 GENERATIONS = 200
 TARGET_SCORE = 50000
+MUTATION_CHANCE = 0.05
 
 
 def generate_individual():
@@ -27,7 +28,6 @@ def initialize():
 
 def cost(ind):
     cost = (ind[0]/ind[1]/ind[2]*ind[3]*ind[4]*ind[5]/ind[6]/ind[7])
-    # print("Indiv: " + str(ind) + " | Cost: " + str(cost))
     # print("Each of this will take 5 minutes")
     return cost
 
@@ -53,7 +53,7 @@ def crossover(pop):
     for x in range (0, len(pop)):
         random_index_table.append(x)
     shuffle(random_index_table)
-    # print(random_index_table)
+
 
     for x in range (0, len(pop), 2):
         p1 = pop[random_index_table[x]][0]
@@ -61,7 +61,6 @@ def crossover(pop):
         cross_point = randint(1, MAX_LAYER)
         pop.append(cross_parents(p1,p2,cross_point))
         pop.append(cross_parents(p2,p1,cross_point))
-
     return pop
 
 
@@ -74,27 +73,50 @@ def calculate_missing_cost(population):
             pop_with_scores.append([x, cost(x)])
     return pop_with_scores
 
-# creates the initial randomized population bag:
-pop_bag = []
-init_pop_bag = initialize()
 
-# initial go through neural network:
-for x in init_pop_bag:
-    pop_bag.append([x, cost(x)])
+def mutate(pop):
+    mutated_pop = []
+    for ind in pop:
+        rand = random()
+        if (rand <= MUTATION_CHANCE):
+            mutated_pop.append(generate_individual())
+            print("MUTEK")
+        else:
+            mutated_pop.append(ind)
+    return mutated_pop
 
-max_score = max(pop_bag, key=lambda x: x[1])[1]
 
-#here it'll check if max_score is enough
+if __name__ == "__main__":
+    # creates the initial randomized population bag:
+    init_pop_bag = initialize()
+    # init_pop_bag=[[2,1,1,1,1,1,1,1],[1,1,1,1,1,1,2,1],[1,1,1,1,1,1,2,1],[1,1,1,1,1,2,1,1],[1,1,1,1,1,1,1,1],[1,2,1,1,1,1,1,1],[1,1,1,1,1,1,1,1],[1,1,1,1,1,1,1,1]]
+    pop_bag = []
 
-for g in range(1, GENERATIONS):
-    print("Generation " + str(g) + " ~~ best score = " + str(max_score))
+    # initial pop go through neural network and setting the score:
+    for x in init_pop_bag:
+        pop_bag.append([x, cost(x)])
+    best_of_gen = max(pop_bag, key=lambda x: x[1])[1]
+    max_score = best_of_gen
 
-    if(max_score) >= TARGET_SCORE: break
+    #loop that goes through rest of generations:
+    for g in range(1, GENERATIONS):
 
-    pop_bag = sort_and_divide(pop_bag)
-    pop_bag = crossover(pop_bag)
-    pop_bag = calculate_missing_cost(pop_bag)
+        #checks if generations maximum was better that top score
+        if max_score < best_of_gen: max_score = best_of_gen
+        print("Generation " + str(g) + " ~~ best score = " + str(round(best_of_gen,2)))
 
-    if max_score < max(pop_bag, key=lambda x: x[1])[1]:
-        max_score = max(pop_bag, key=lambda x: x[1])[1]
+        #stop condtiion check
+        if(max_score) >= TARGET_SCORE:
+            print("Target of score = " + str(TARGET_SCORE) + " was accomplished.")
+            break
 
+        #sorting the list and getting top 50%
+        pop_bag = sort_and_divide(pop_bag)
+        #crossover of randomly selected parents
+        pop_bag = crossover(pop_bag)
+        #mutating
+        pop_bag = mutate(pop_bag)
+        #calculating score for new children and mutated ones
+        pop_bag = calculate_missing_cost(pop_bag)
+        #getting best score of generation
+        best_of_gen = max(pop_bag, key=lambda x: x[1])[1]
